7c88b11c3d37de050005caa3d1e1006e03641985
Tomi <pintertamas99@gmail.com>
2025-03-05 00:28:31 +0100

    feat: initialize maven project scaffold in IntelliJ

---
+++ src/main/java/com/tamaspinter/instantpaymentapi/InstantPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.tamaspinter.instantpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantPaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamaspinter/instantpaymentapi/InstantPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamaspinter.instantpaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantPaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
