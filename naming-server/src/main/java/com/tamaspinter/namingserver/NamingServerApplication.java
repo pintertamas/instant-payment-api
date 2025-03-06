7b3a889b7781f25ed647a080864a32522d7f0d54
Tomi <pintertamas99@gmail.com>
2025-03-07 00:28:41 +0100

    fix: update NamingServer application name

--- naming-server/src/main/java/com/tamaspinter/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/tamaspinter/namingserver/NamingServerApplication.java
@@ -2,11 +2,12 @@ package com.tamaspinter.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class NamingServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(NamingServerApplication.class, args);
     }
 }
